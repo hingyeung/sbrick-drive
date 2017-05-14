3eedc814419e921294b3e58b8e6f03f7eba9fb5d
Samuel Li <samli@samuelli.net>
2017-05-14 23:17:28 +1000

    More refactoring. Added power to SBCommand

--- main.js
+++ main.js
@@ -8,19 +8,90 @@ const SBController = require('./lib/SBrickController'),
 var sbController = new SBController();
 sbController.init();
 sbController.on(SBEvent.CONNECTED, function() {
-	// drive forward on channel 0 and 1
 	logger.info('SBController reported CONNECTED event');
-		sbController.queueCommand(
-		new SBCommand(
-			SBCommand.TYPE.FORWARD, [0, 1]));
 
+	forward();
+	noop();
+	backward();
+	noop();
+	stop();
+
+	// forward();
+	// left();
+	// noop();
+	// right();
+	// noop();
+	// straight();
+	// stop();
+});
+
+function left() {
+	sbController.queueCommand(
+		new SBCommand({
+			commandType: SBCommand.TYPE.DRIVE,
+			channels: [2],
+			direction: SBCommand.DIRECTION.FORWARD,
+			power: SBCommand.POWER_SETTING.FULL_POWER
+		}));
+}
+
+function right() {
+	sbController.queueCommand(
+		new SBCommand({
+			commandType: SBCommand.TYPE.DRIVE,
+			channels: [2],
+			direction: SBCommand.DIRECTION.BACKWARD,
+			power: SBCommand.POWER_SETTING.FULL_POWER
+		}));
+}
+
+function straight() {
+	sbController.queueCommand(
+		new SBCommand({
+			commandType: SBCommand.TYPE.DRIVE,
+			channels: [2],
+			direction: SBCommand.DIRECTION.FORWARD,
+			power: SBCommand.POWER_SETTING.NO_POWER
+		}));
+}
+
+function forward() {
+	// drive forward on channel 0 and 1
+	sbController.queueCommand(
+		new SBCommand({
+			commandType: SBCommand.TYPE.DRIVE,
+			channels: [0, 1],
+			direction: SBCommand.DIRECTION.FORWARD,
+			power: SBCommand.POWER_SETTING.FULL_POWER
+		}));
+}
+
+function backward() {
+	// drive backward on channel 0 and 1
+	sbController.queueCommand(
+		new SBCommand({
+			commandType: SBCommand.TYPE.DRIVE,
+			channels: [0, 1],
+			direction: SBCommand.DIRECTION.BACKWARD,
+			power: SBCommand.POWER_SETTING.FULL_POWER
+		}));
+}
+
+function noop() {
 	// no-op, continue with the previous command
 	sbController.queueCommand(
-		new SBCommand(
-			SBCommand.TYPE.NOOP, [0,1], 2000));
+		new SBCommand({
+			commandType: SBCommand.TYPE.NOOP,
+			channels: [0,1],
+			duration: 2000
+		}));
+}
 
+function stop() {
 	// stop all channel
 	sbController.queueCommand(
-		new SBCommand(
-			SBCommand.TYPE.STOP, [0,1]));
-});
+		new SBCommand({
+			commandType: SBCommand.TYPE.STOP,
+			channels: [0,1]
+		}));
+}
