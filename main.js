13729b7811a89d3046c19a2f794291a2dc7eb1ef
Samuel Li <samli@samuelli.net>
2017-05-16 00:48:20 +1000

    implemented a command queue for each channel

--- main.js
+++ main.js
@@ -3,7 +3,11 @@
 const SBController = require('./lib/SBrickController'),
 		logger = require('./lib/Logger'),
 		SBEvent = require('./lib/SBEvent'),
+		_ = require('lodash'),
 		SBCommand = require('./lib/SBCommand');
+
+const DRIVE_CHANNELS = [0, 1],
+	  STEERING_CHANNELS = [2];
 
 var sbController = new SBController();
 sbController.init();
@@ -11,87 +15,109 @@ sbController.on(SBEvent.CONNECTED, function() {
 	logger.info('SBController reported CONNECTED event');
 
 	forward();
-	noop();
-	backward();
-	noop();
+	maintainSpeed(2000);
+	left();
+	maintainDirection(2000);
+	straight();
+	maintainSpeed(2000);
+	right();
+	maintainDirection(2000);
+	straight();
+	maintainSpeed(2000);
 	stop();
-
-	// forward();
-	// left();
-	// noop();
-	// right();
-	// noop();
-	// straight();
-	// stop();
 });
 
+// TODO change to 1 chanenl
 function left() {
-	sbController.queueCommand(
-		new SBCommand({
-			commandType: SBCommand.TYPE.DRIVE,
-			channels: [2],
-			direction: SBCommand.DIRECTION.FORWARD,
-			power: SBCommand.POWER_SETTING.FULL_POWER
-		}));
+	_.forEach(STEERING_CHANNELS, function(channel) {
+		sbController.queueCommand(
+			new SBCommand({
+				commandType: SBCommand.TYPE.DRIVE,
+				channel: channel,
+				direction: SBCommand.DIRECTION.FORWARD,
+				power: SBCommand.POWER_SETTING.FULL_POWER
+			}));
+	});
 }
 
 function right() {
-	sbController.queueCommand(
-		new SBCommand({
-			commandType: SBCommand.TYPE.DRIVE,
-			channels: [2],
-			direction: SBCommand.DIRECTION.BACKWARD,
-			power: SBCommand.POWER_SETTING.FULL_POWER
-		}));
+	_.forEach(STEERING_CHANNELS, function(channel) {
+		sbController.queueCommand(
+			new SBCommand({
+				commandType: SBCommand.TYPE.DRIVE,
+				channel: channel,
+				direction: SBCommand.DIRECTION.BACKWARD,
+				power: SBCommand.POWER_SETTING.FULL_POWER
+			}));
+	});
 }
 
 function straight() {
-	sbController.queueCommand(
-		new SBCommand({
-			commandType: SBCommand.TYPE.DRIVE,
-			channels: [2],
-			direction: SBCommand.DIRECTION.FORWARD,
-			power: SBCommand.POWER_SETTING.NO_POWER
-		}));
+	_.forEach(STEERING_CHANNELS, function(channel) {
+		sbController.queueCommand(
+			new SBCommand({
+				commandType: SBCommand.TYPE.DRIVE,
+				channel: channel,
+				direction: SBCommand.DIRECTION.FORWARD,
+				power: SBCommand.POWER_SETTING.NO_POWER
+			}));
+	});
 }
 
 function forward() {
-	// drive forward on channel 0 and 1
-	sbController.queueCommand(
-		new SBCommand({
-			commandType: SBCommand.TYPE.DRIVE,
-			channels: [0, 1],
-			direction: SBCommand.DIRECTION.FORWARD,
-			power: SBCommand.POWER_SETTING.FULL_POWER
-		}));
+	_.forEach(DRIVE_CHANNELS, function(channel) {
+		sbController.queueCommand(
+			new SBCommand({
+				commandType: SBCommand.TYPE.DRIVE,
+				channel: channel,
+				direction: SBCommand.DIRECTION.FORWARD,
+				power: SBCommand.POWER_SETTING.FULL_POWER
+			})
+		)
+	});
 }
 
 function backward() {
 	// drive backward on channel 0 and 1
-	sbController.queueCommand(
-		new SBCommand({
-			commandType: SBCommand.TYPE.DRIVE,
-			channels: [0, 1],
-			direction: SBCommand.DIRECTION.BACKWARD,
-			power: SBCommand.POWER_SETTING.FULL_POWER
+	_.forEach(DRIVE_CHANNELS, function(channel) {
+		sbController.queueCommand(
+			new SBCommand({
+				commandType: SBCommand.TYPE.DRIVE,
+				channel: channel,
+				direction: SBCommand.DIRECTION.BACKWARD,
+				power: SBCommand.POWER_SETTING.FULL_POWER
 		}));
+	});
 }
 
-function noop() {
-	// no-op, continue with the previous command
-	sbController.queueCommand(
-		new SBCommand({
-			commandType: SBCommand.TYPE.NOOP,
-			channels: [0,1],
-			duration: 2000
-		}));
+function maintainSpeed(duration) {
+	_.forEach(DRIVE_CHANNELS, function(channel) {
+		sbController.queueCommand(
+			new SBCommand({
+				commandType: SBCommand.TYPE.NOOP,
+				channel: channel,
+				duration: duration
+			}));
+	});
+}
+
+function maintainDirection(duration) {
+	_.forEach(STEERING_CHANNELS, function(channel) {
+		sbController.queueCommand(
+			new SBCommand({
+				commandType: SBCommand.TYPE.NOOP,
+				channel: channel,
+				duration: duration
+			}));
+	});
 }
 
 function stop() {
-	// stop all channel
-	sbController.queueCommand(
-		new SBCommand({
-			commandType: SBCommand.TYPE.STOP,
-			channels: [0,1]
-		}));
+	_.forEach(DRIVE_CHANNELS, function(channel) {
+		sbController.queueCommand(
+			new SBCommand({
+				commandType: SBCommand.TYPE.STOP,
+				channel: channel
+			}));
+	});
 }
