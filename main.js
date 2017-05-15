0f2e927a3b4eb9cb6d47bcf9eefa631687204db0
Samuel Li <samli@samuelli.net>
2017-05-16 01:34:45 +1000

    added a keyboard interface.

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const SBController = require('./lib/SBrickController'),
 		logger = require('./lib/Logger'),
 		SBEvent = require('./lib/SBEvent'),
 		_ = require('lodash'),
-		SBCommand = require('./lib/SBCommand');
+		SBCommand = require('./lib/SBCommand'),
+		readline = require('readline');
 
 const DRIVE_CHANNELS = [0, 1],
 	  STEERING_CHANNELS = [2],
@@ -15,12 +16,43 @@ sbController.init();
 sbController.on(SBEvent.CONNECTED, function() {
 	logger.info('SBController reported CONNECTED event');
 
-	driveFor(DIRECTION.FORWARD, 2000);
-	driveFor(DIRECTION.LEFT, 2000);
-	driveFor(DIRECTION.RIGHT, 2000);
-	driveFor(DIRECTION.FORWARD, 2000);
-	stop();
+	// driveFor(DIRECTION.FORWARD, 2000);
+	// driveFor(DIRECTION.LEFT, 2000);
+	// driveFor(DIRECTION.RIGHT, 2000);
+	// driveFor(DIRECTION.FORWARD, 2000);
+	// driveFor(DIRECTION.BACKWARD, 5000);
+	// driveFor(DIRECTION.BACKWARD, 3000);
+	// stop();
+	enableKeyboardControl();
 });
+
+function enableKeyboardControl() {
+	readline.emitKeypressEvents(process.stdin);
+	process.stdin.setRawMode(true);
+
+	process.stdin.on('keypress', (str, key) => {
+	  if (key.ctrl && key.name === 'c') {
+	  	logger.info('Exiting...')
+	    process.exit();
+	  } else {
+	    logger.debug('You pressed the %s key: %j', str, key);
+	    if (key.name === 'up') {
+	    	driveFor(DIRECTION.FORWARD, 1000);
+	    	stop();
+	    } else if (key.name === 'left') {
+	    	driveFor(DIRECTION.LEFT, 1000);
+	    	stop();
+	    } else if (key.name === 'right') {
+	    	driveFor(DIRECTION.RIGHT, 1000);
+	    	stop();
+	    } else if (key.name === 'down') {
+	    	driveFor(DIRECTION.BACKWARD, 1000);
+	    	stop();
+	    }
+	  }
+	});
+	console.log('Press UP, DOWN, LEFT or RIGHT...');
+}
 
 function driveFor(direction, duration) {
 	switch (direction) {
