ef935bfa37ef92ef08895fab58950e03a587ac21
Samuel Li <samli@samuelli.net>
2017-05-18 22:38:49 +1000

    added dedicated script for loading instructions from text file.

---
+++ drive_by_script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const readline = require('linebyline'),
+	  logger = require('./lib/Logger'),
+	  SimpleDriveController = require('./lib/SimpleDriveController');
+
+const DRIVE_CHANNELS = [0, 1],
+	  STEERING_CHANNELS = [2];
+
+var simpleDriveController = new SimpleDriveController(DRIVE_CHANNELS, STEERING_CHANNELS);
+simpleDriveController.connect(executeStepsInFile);
+
+function executeStepsInFile() {
+	const filename = './data/instructions.txt';
+	var rl = readline(filename);
+	rl.on('line', function(line, lineCount, byteCount) {
+		if (!line.trim().startsWith('#')) {
+			sendCommandToSBrick(line);
+		}
+	}).on('error', function(e) {
+    	throw new Error(e);
+  	});
+}
+
+function sendCommandToSBrick(commandStr) {
+	logger.debug('Sending %s command to SBrick', commandStr);
+    if (commandStr === 'forward') {
+    	simpleDriveController.simpleForward();
+    } else if (commandStr === 'left') {
+    	simpleDriveController.simpleLeft();
+    } else if (commandStr === 'right') {
+    	simpleDriveController.simpleRight();
+    } else if (commandStr === 'backward') {
+    	simpleDriveController.simpleBackward();
+    }
+}
